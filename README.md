ce8d219e2bd4875c13738b6fd11f649603325ff4
Fergoth <fergothew@gmail.com>
2019-04-12 16:41:34 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+Script that let you check your online friends from console.
 
 # How to Install
 
@@ -11,6 +11,20 @@ pip install -r requirements.txt # alternatively try pip3
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+# How to use
+* First you need to register your own app in https://vk.com/dev
+* Set APP_ID to your own app id
+* Then launch application and input your login and password
+```bash
+>python vk_friends_online.py
+>Введите имя пользователя вк
+>[username]
+>Password:[password]
+```
+# Output
+Друзья онлайн:  
+[name] [second name]
+
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
+import getpass
+import sys
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6941791  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    username = input('Введите имя пользователя вк\n')
+    return username
 
 
 def get_user_password():
-    pass
+    return getpass.getpass()
 
 
 def get_online_friends(login, password):
@@ -18,15 +20,27 @@ def get_online_friends(login, password):
         user_login=login,
         user_password=password,
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api = vk.API(session, v='5.95')
+    friends_info = api.friends.get(fields='online')['items']
+    friends_online = [
+        (friend['first_name'], friend['last_name'])
+        for friend in friends_info
+        if friend['online']
+    ]
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Друзья онлайн: ')
+    for name, second_name in friends_online:
+        print('{:^20}{:^20}'.format(name, second_name))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError:
+        sys.exit('Ошибка авторизации')
     output_friends_to_console(friends_online)
