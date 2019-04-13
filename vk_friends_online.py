704ded4d21471d6c65e34787c8e7e0eb441124a8
Fergoth <fergothew@gmail.com>
2019-04-13 12:29:43 +0300

    some fixes

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -2,7 +2,7 @@ import vk
 import getpass
 import sys
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = -1
 
 
 def get_user_login():
@@ -14,14 +14,14 @@ def get_user_password():
     return getpass.getpass()
 
 
-def get_online_friends(login, password):
+def get_online_friends(login, password, version='5.95'):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
         scope=2,
     )
-    api = vk.API(session, v='5.95')
+    api = vk.API(session, v=version)
     friends_online_ids = api.friends.getOnline()
     friends_online  = api.users.get(user_ids=friends_online_ids)
     return friends_online
@@ -34,7 +34,7 @@ def output_friends_to_console(friends_online):
 
 
 if __name__ == '__main__':
-    #ogin = get_user_login()
+    login = get_user_login()
     password = get_user_password()
     try:
         friends_online = get_online_friends(login, password)
