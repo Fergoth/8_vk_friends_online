c737b54c399c574fc7f8c1c2b76c9cd52e8a6ef3
Fergoth <fergothew@gmail.com>
2019-04-13 10:43:37 +0300

    Strange Valli fix

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -19,25 +19,22 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope=2,
     )
     api = vk.API(session, v='5.95')
-    friends_info = api.friends.get(fields='online')['items']
-    friends_online = [
-        (friend['first_name'], friend['last_name'])
-        for friend in friends_info
-        if friend['online']
-    ]
+    friends_online_ids = api.friends.getOnline()
+    friends_online  = api.users.get(user_ids=friends_online_ids)
     return friends_online
 
 
 def output_friends_to_console(friends_online):
     print('Друзья онлайн: ')
-    for name, second_name in friends_online:
-        print('{:^20}{:^20}'.format(name, second_name))
+    for friend in friends_online:
+        print('{:^20}{:^20}'.format(friend['first_name'], friend['last_name']))
 
 
 if __name__ == '__main__':
-    login = get_user_login()
+    #ogin = get_user_login()
     password = get_user_password()
     try:
         friends_online = get_online_friends(login, password)
